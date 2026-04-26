26924915f1b411095371422fc95271e17165a2b6
nguyenanhtu <nguyenanhtu16042k5@gmail.com>
2026-04-26 22:26:29 +0700

    feat: add throw exception in get student behavior log history

--- src/Application/BehaviorLogs/Queries/GetStudentBehaviorLogHistory/GetStudentBehaviorLogHistory.cs
+++ src/Application/BehaviorLogs/Queries/GetStudentBehaviorLogHistory/GetStudentBehaviorLogHistory.cs
@@ -2,12 +2,27 @@
 
 public record GetStudentBehaviorLogHistoryQuery(int StudentId, int LessonId) : IRequest<StudentBehaviorLogHistoryVm>;
 
-public class GetStudentBehaviorLogHistoryQueryHandler(IApplicationDbContext context)
+public class GetStudentBehaviorLogHistoryQueryHandler(IApplicationDbContext context, ICurrentTeacher currentTeacher)
     : IRequestHandler<GetStudentBehaviorLogHistoryQuery, StudentBehaviorLogHistoryVm>
 {
     public async Task<StudentBehaviorLogHistoryVm> Handle(GetStudentBehaviorLogHistoryQuery request,
         CancellationToken cancellationToken)
     {
+        Lesson? lesson = await context.Lessons
+            .Where(l => l.Id == request.LessonId)
+            .Include(l => l.TeachingContext)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (lesson == null)
+        {
+            throw new NotFoundException($"Lesson with id {request.LessonId} was not found");
+        }
+
+        if (lesson.TeachingContext!.TeacherId != int.Parse(currentTeacher.Id!))
+        {
+            throw new ForbiddenAccessException("You don't have permission to access this resources");
+        }
+
         List<BehaviorLog> logs = await context.BehaviorLogs
             .AsNoTracking()
             .Where(x => x.StudentId == request.StudentId && x.LessonId == request.LessonId)
